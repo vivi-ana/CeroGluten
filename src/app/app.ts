c0e88cbb83bca16c5b7d3b5990164ef4c25d6ed7
Viviana <acostaviviana.a@gmail.com>
2025-07-29 21:05:01 -0300

    feat: add hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,5 +12,12 @@ import { Contact } from './contact/contact';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Cero Gluten'); 
+  protected readonly title = signal('Cero Gluten');
+
+  scrollToLocals() {
+    const el = document.getElementById('locales');
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
